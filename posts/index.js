498cf40990abc261b80a6152707c193699c44ec8
Aveej17 <jeevaboobathi1718@gmail.com>
2024-06-16 22:01:10 +0530

    first commit

---
+++ posts/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+const cors = require('cors');
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const posts = {};
+
+app.get('/posts', (req, res)=>{
+    res.send(posts);
+
+});
+
+app.post('/posts', (req, res)=>{
+    const id = randomBytes(4).toString('hex');
+    const { title }  = req.body;
+    console.log(title);
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+
+
+});
+
+app.listen(4000, ()=>{
+    console.log(`Posts Listenning on 4000`);
+})
